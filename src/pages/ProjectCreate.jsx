6ca3e22909a96a68db5d62b95ee8487289d995d5
alex <alexparaschiv448@gmail.com>
2026-04-09 14:33:25 +0300

    removed unused nav

--- src/pages/ProjectCreate.jsx
+++ src/pages/ProjectCreate.jsx
@@ -1,10 +1,8 @@
 import '../index.css'
 import PageLayout from "../components/PageLayout";
-import {useNavigate} from "react-router-dom";
 import CreateProjectForm from "../components/CreateProjectForm";
 
 export default function Projects() {
-    let nav=useNavigate();
     return(
         <>
             <PageLayout>
